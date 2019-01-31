4950e9f26c4fca6e23caefc20dec94187a21a35f
Niwash Acharya <acharyaniwash19@gmail.com>
2019-01-31 15:01:00 +0530

    Add files via upload

---
+++ art/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      },700);
+    });
+    
+  })
